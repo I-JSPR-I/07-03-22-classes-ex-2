420a447e6f2424a44283bfa3e2e4583ddee259a5
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-03-10 14:55:40 +0100

    Add project files.

---
+++ 07-03-22 classes ex 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _07_03_22_classes_ex_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                PuntenBoek Punt;
+                List<PuntenBoek> Punten = new List<PuntenBoek>();
+                do
+                {
+                    Punt = new PuntenBoek();
+
+
+
+                    Console.WriteLine("Wilt u nog punten toevoegen?(J/N)");
+                } while (Console.ReadLine().ToUpper().Substring(0, 1) == "J");
+
+            }
+        }
+    }
+}
---
+++ 07-03-22 classes ex 2/PuntenBoek.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07_03_22_classes_ex_2
+{
+    class PuntenBoek
+    {
+        public int Punt;
+        public int totaal;
+        public int i = 0;
+        public PuntenBoek()
+        {
+            Punt = 0;
+            totaal = 0;
+            i = 0;
+        }
+
+        public PuntenBoek(int IntPunten)
+        {
+            Punt = IntPunten;
+            totaal = totaal + Punt;
+            i++;
+
+        }
+    }
+}
